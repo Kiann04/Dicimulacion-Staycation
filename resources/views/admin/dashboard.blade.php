8a905af537993b3f58101db491c278c3bdcd7304
Kiann04 <kiansantos06@gmail.com>
2025-09-29 05:10:38 -0700

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -58,7 +58,7 @@
                 <tbody>
                 @forelse($bookings as $booking)
                     <tr id="booking-{{ $booking->id }}">
-                        <td>{{ $booking->id }}</td>
+                        <td>{{ $booking->staycation->house_name ?? 'N/A' }}</td>
                         <td>{{ $booking->staycation_id }}</td>
                         <td>{{ $booking->name }}</td>
                         <td>{{ $booking->phone }}</td>
