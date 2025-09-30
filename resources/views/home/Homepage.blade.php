15fed244261a1352a4e6fc0206bcfae83128af19
Kiann04 <kiansantos06@gmail.com>
2025-09-30 02:05:25 -0700

    Update Homepage.blade.php

--- resources/views/home/Homepage.blade.php
+++ resources/views/home/Homepage.blade.php
@@ -48,6 +48,7 @@
         </button>
     </div>
 </section>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 <!-- About Us -->
 <section class="container my-5" id="about">
