b3f04418c93e5124d6721469d897dba20d54d842
Kiann04 <kiansantos06@gmail.com>
2025-10-05 04:49:40 -0700

    Update Booking.php

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -14,20 +14,12 @@ class Booking extends Model
         'staycation_id',
         'user_id',
         'name',
-        'email',
         'phone',
+        'status',
         'guest_number',
         'start_date',
         'end_date',
-        'price_per_day',
-        'total_price',
-        'amount_paid',
-        'payment_status',
-        'payment_method',
-        'payment_proof',
-        'transaction_number',
-        'message_to_admin',
-        'status',
+        'total_price'
     ];
 
     public function staycation()
@@ -44,14 +36,13 @@ class Booking extends Model
     {
         parent::boot();
 
-        // Automatically calculate total_price before saving
+        // Automatically calculate total_price when saving
         static::saving(function ($booking) {
             if ($booking->staycation) {
                 // Number of nights excluding the departure date
                 $nights = Carbon::parse($booking->start_date)
-                               ->diffInDays(Carbon::parse($booking->end_date));
+                            ->diffInDays(Carbon::parse($booking->end_date));
                 $booking->total_price = $nights * $booking->staycation->house_price;
-                $booking->price_per_day = $booking->staycation->house_price;
             }
         });
     }
