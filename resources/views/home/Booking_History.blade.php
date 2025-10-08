0ffa744be2b6ca0f783f80799667a0fe444563e9
Kiann04 <kiansantos06@gmail.com>
2025-10-07 17:25:36 -0700

    Update Booking_History.blade.php

--- resources/views/home/Booking_History.blade.php
+++ resources/views/home/Booking_History.blade.php
@@ -4,7 +4,7 @@
 @include('Header')
 @endsection
 
-<main class="flex-grow-1"style="padding-top: 100px;">
+<main class="flex-grow-1"style="padding-top: 50px;">
 <div class="container my-5">
     <h3 class="fw-bold mb-4">Your Booking History</h3>
 
