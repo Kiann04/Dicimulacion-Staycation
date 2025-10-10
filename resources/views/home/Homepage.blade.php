78b3fdd021536f93aee65de4e5cce1c808353614
Kiann04 <kiansantos06@gmail.com>
2025-10-10 08:58:47 +0800

    Update Homepage.blade.php

--- resources/views/home/Homepage.blade.php
+++ resources/views/home/Homepage.blade.php
@@ -131,9 +131,6 @@
 }
 </style>
 
-<!-- Bootstrap CSS -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-
 <!-- Bootstrap Bundle JS (includes Popper) -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
