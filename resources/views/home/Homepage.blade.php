2a8b13c461f3de64767511c91b140de8e334a3c4
Kiann04 <kiansantos06@gmail.com>
2025-10-10 10:20:40 +0800

    Update Homepage.blade.php

--- resources/views/home/Homepage.blade.php
+++ resources/views/home/Homepage.blade.php
@@ -131,8 +131,6 @@
 }
 </style>
 
-<!-- Bootstrap Bundle JS (includes Popper) -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
 <!-- Modern About Us Section -->
 <section class="container my-5 py-5" id="about">
