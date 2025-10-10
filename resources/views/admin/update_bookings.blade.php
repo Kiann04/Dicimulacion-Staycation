a994c96d02d153cfaf6b7ba6db7aec316cddce9b
Kiann04 <kiansantos06@gmail.com>
2025-10-10 16:09:25 +0800

    Update update_bookings.blade.php

--- resources/views/admin/update_bookings.blade.php
+++ resources/views/admin/update_bookings.blade.php
@@ -48,6 +48,7 @@
 </style>
 <body class="admin-dashboard">
 <div class="content-wrapper">
+<div class="update-booking-container">
     <div class="update-booking-card">
         <h2>✏️ Update Booking</h2>
 
