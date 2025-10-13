3fccfaee4cd8431e7bcdae3d0072d9171667a654
Kiann04 <kiansantos06@gmail.com>
2025-10-13 16:08:45 +0800

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,11 +1,10 @@
 @extends('layouts.default')
-
+@include('admin.partials.analytics')
 @section('Aside')
     @include('Aside')
 @endsection
 
 @section('content')
-@include('admin.partials.analytics')
 <body class="admin-dashboard">
     <div class="content-wrapper">
         <div class="main-content">
