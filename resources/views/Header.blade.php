bccf5da50a2ec542f79c9fdb5f2db1c392934050
Kiann04 <kiansantos06@gmail.com>
2025-10-14 14:21:55 +0800

    Update Header.blade.php

--- resources/views/Header.blade.php
+++ resources/views/Header.blade.php
@@ -22,7 +22,7 @@
                         <a class="nav-link" href="{{ route('home') }}#properties">Houses</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('home') }}#about">About Us</a>
+                        <a class="nav-link" href="{{ route('home') }}#about">Our Story</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('home') }}#contact">Contact Us</a>
