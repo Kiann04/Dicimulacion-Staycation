bd675d9d3ee54558c6d3f8826ef45d4bd7dc4c00
Kiann04 <kiansantos06@gmail.com>
2025-10-18 13:46:30 +0800

    Update cancelled.blade.php

--- resources/views/admin/cancelled.blade.php
+++ resources/views/admin/cancelled.blade.php
@@ -6,6 +6,7 @@
 @section('content')
 <body class="admin-dashboard">
 <div class="content-wrapper">
+    <div class="main-content">
     <h2><i class="fa-solid fa-ban text-danger"></i> Cancelled Bookings</h2>
     <table class="table table-bordered mt-3">
         <thead>
