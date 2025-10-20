48ec93251e388e187dfc89ecd0c8c8c36e8e0d53
Kiann04 <kiansantos06@gmail.com>
2025-10-21 01:42:21 +0800

    Update CancelledBookingController.php

--- app/Http/Controllers/CancelledBookingController.php
+++ app/Http/Controllers/CancelledBookingController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
+use App\Models\BookingHistory;
 
 class CancelledBookingController extends Controller
 {
     public function index()
     {
-        $cancelledBookings = DB::table('booking_history')
-            ->orderBy('deleted_at', 'desc')
+        $cancelledBookings = BookingHistory::with('staycation')
+            ->orderByDesc('deleted_at')
             ->get()
             ->map(function ($booking) {
                 $booking->formatted_start_date = Carbon::parse($booking->start_date)->format('M d, Y');
